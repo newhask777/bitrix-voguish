2b52a3d79d69aa3fdb1cdbd4e5b3bf2099e382a6
newhask777 <newhaskellisp@gmail.com>
2024-05-20 09:59:10 +0300

    Страница делаьного просмотра блога

--- blog/index.php
+++ blog/index.php
@@ -22,8 +22,9 @@ $APPLICATION->SetTitle("Блог");
 		"DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",
 		"DETAIL_DISPLAY_TOP_PAGER" => "N",
 		"DETAIL_FIELD_CODE" => array(
-			0 => "",
-			1 => "",
+			0 => "DETAIL_PICTURE",
+			1 => "SHOW_COUNTER",
+			2 => "",
 		),
 		"DETAIL_PAGER_SHOW_ALL" => "Y",
 		"DETAIL_PAGER_TEMPLATE" => "",
